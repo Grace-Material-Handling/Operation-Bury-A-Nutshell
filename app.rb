e5b5c13470f216745fd7cba043b7550d2cf7d61e
christianjuth <cjuth2@gmail.com>
2015-08-03 14:17:58 -0400

    print backup url to console

--- app.rb
+++ app.rb
@@ -5,3 +5,17 @@ $nutshell = NutshellCrmAPI::Client.new($username, $apiKey)
 
 # Backup Nutshell
 $nutshell.new_backup()
+
+# Wait until backup is done
+backup = {}
+loop do
+	backup = $nutshell.find_backups[0]
+	if backup['status'].to_i == 1
+		break
+	else
+		sleep(5)
+	end
+end
+
+# Print backup link to console
+puts "You can download this backup at #{backup['file']['uri']}."
